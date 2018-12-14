e452f8f1da7d88452f31d6cf778478d52f03c337
Vladimir Biro <vladimir.biro@gmail.com>
2018-12-14 20:32:57 +0100

    First commit

---
+++ src/AllocateRamControl.php
@@ -0,0 +1,37 @@
+<?php
+namespace VB\Services;
+
+use Nette\Utils\Strings;
+use Tracy\Debugger;
+
+class AllocateRamControl
+{
+	private $switch = 0;
+	private $minRam = false;
+
+	public function log($caption = '', $minRam = false)
+	{
+		if ($this->switch == 0) {
+			return;
+		}
+		$size = memory_get_usage();
+		$ram = @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2);
+		if ($minRam === false || ($size/1024/1024) > $minRam) {
+			$unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
+			$r = $ram . ' ' . $unit[$i];
+			Debugger::log(Strings::padRight($caption . '   ', 30, '-') . '->   ' . Strings::padRight($r . ' ', 10, ' ') . '|');
+		}
+	}
+
+
+	public function setSwitch($switch)
+	{
+		$this->switch = $switch;
+	}
+
+
+	public function setMinRam($minRam)
+	{
+		$this->minRam = $minRam;
+	}
+}
